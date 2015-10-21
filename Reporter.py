ee8843e155f6a7cb14b2d1da2abb220809a9f94e
Ryan Clair <ryan.clair@gmail.com>
2015-10-21 09:36:17 -0500

    Logger + Bug Fixes
    Added a logger to commit any errors to “SRDFA_Reporter_Error_Log.csv”
    so errors are caught if script is ran on a scheduled basis.
    
    Fixed SG’s API URL and included logic to get around various errors.

--- Reporter.py
+++ Reporter.py
@@ -4,6 +4,40 @@ import requests, json, time, logging, os, sys, csv
 from datetime import datetime, date, timedelta
 from datetime import time as goofytime
 
+def logger(EL, MSG):
+    DATE = datetime.today()
+
+    if os.path.isfile('SRDFA_Reporter_Error_Log.csv') == False:
+
+        with open('SRDFA_Reporter_Error_Log.csv','w') as csvfile:
+            writer = csv.writer(csvfile, delimiter=',',
+                                    quotechar=',', quoting=csv.QUOTE_MINIMAL)
+            writer.writerow(['Date',"Error Level",'Error'])
+            writer.writerow([DATE,EL,MSG])
+
+
+    else:
+        with open('SRDFA_Reporter_Error_Log.csv','a') as csvfile:
+            writer = csv.writer(csvfile, delimiter=',',
+                                    quotechar=',', quoting=csv.QUOTE_MINIMAL)
+            writer.writerow([DATE,EL,MSG])
+
+    if EL == "Informational":
+        logging.info(MSG)
+    elif EL == "Warning":
+        logging.warning(MSG)
+    elif EL == "Critical":
+        logging.critical(MSG)
+
+def payload(SYM, SG):
+    PAYLOAD = {
+        "startDate" : unix_ym,
+        "endDate" : unix_midnight,
+        "symmetrixId" : SYM,
+        "storageGroupId" : SG,
+        "metrics" : ["SRDFA_MBSent"]
+    }
+    return PAYLOAD
 
 # Disable warnings from untrusted server certificates
 try:
@@ -19,20 +53,20 @@ TIMESTAMP = date.today() - timedelta(days=1)
 try:
     LOCATION = os.environ['SRUNILOCATION']
 except KeyError:
-    logging.critical('Please set the environment variable SRUNILOCATION to IP:Port of Unisphere using "export SRUNILOCATION=IP:PORT" command')
+    logger("Critical",'Need an IP:Port. Please set the environment variable SRUNILOCATION to IP:Port of Unisphere using either "export SRUNILOCATION=IP:PORT" (linux command) or "set SRUINLOCATION=IP:PORT" (Windows command)')
     sys.exit(1)
 
 # Get credentials for Unisphere login from environmentals
 try:
     USER = os.environ['SRUSER']
 except KeyError:
-    logging.critical('Please set the environment variable SRUSER to a valid username for Unisphere using "export SRUSER=user" command')
+    logger("Critical",'Need a username. Please set the environment variable SRUSER to the username of Unisphere using either "export SRUSER=username" (linux command) or "set SRUSER=username" (Windows command)')
     sys.exit(1)
 
 try:
     PASS = os.environ['SRPASS']
 except KeyError:
-    logging.critical('Please set the environment variable SRPASS to the password for the username for Unisphere using "export SRPASS=password" command')
+    logger("Critical",'Need a password. Please set the environment variable SRPASS to the password of Unisphere using either "export SRUSER=password" (linux command) or "set SRUSER=password" (Windows command)')
     sys.exit(1)
 
 # Get current directory the script is running in
@@ -44,15 +78,6 @@ if os.path.isfile('SRDFA_Reporter.csv') == False:
                                 quotechar=',', quoting=csv.QUOTE_MINIMAL)
         writer.writerow(['24h Period','Array','Storage Group','Total MB Sent by SRDFA'])
 
-def payload(SYM, SG):
-    PAYLOAD = {
-        "startDate" : unix_ym,
-        "endDate" : unix_midnight,
-        "symmetrixId" : SYM,
-        "storageGroupId" : SG,
-        "metrics" : ["SRDFA_MBSent"]
-    }
-    return (PAYLOAD)
 
 
 #Get today's midnight
@@ -75,23 +100,34 @@ unix_ym = ('%f' % unix_ym).rstrip('0').rstrip('.')
 
 # Get all VMAXs for a given Unisphere Instance
 vURL = "https://" + LOCATION + "/univmax/restapi/provisioning/symmetrix"
+
 try:
     vr = requests.get(vURL, auth=(USER,PASS), verify = False)
     DATA=vr.json()
     SYML = (DATA["symmetrixId"])
 except ValueError:
-    logging.critical('Login failure. SRUSER and SRPASS credentials failed. Please confirm the environmental variables are properly set.')
+    logger("Critical",'Login failure. $SRUSER and $SRPASS credentials failed. Please confirm the environmental variables are properly set with correct username/password')
     sys.exit(1)
 except Exception as e:
-    logging.critical('IP:Port information is incorrect. Please confirm SRUNILOCATION environment variable is properly set. Detailed Error: {0}'.format(e))
+    logger("Critical",'IP:Port information is incorrect. Please confirm SRUNILOCATION environment variable is properly set and Unisphere is up and running. Detailed Error: {0}'.format(e))
     sys.exit(1)
 
 # For each VMAX in Unisphere, get the storage Groups
 for SYM in SYML:
-    sURL = "https://" + LOCATION + "/univmax/restapi/provisioning/symmetrix" + SYM + "/storagegroup"
+    sURL = "https://" + LOCATION + "/univmax/restapi/provisioning/symmetrix/" + SYM + "/storagegroup"
+
     sr = requests.get(sURL, auth=(USER,PASS), verify = False)
     sresponse = sr.json()
-    SGL = sresponse["storageGroupId"]
+
+    # Check if a message was returned. If there is a message, report the message and then skip to the next VMAX.
+    test = sresponse.get('message')
+    if test != None:
+        MESSAGE = sresponse.get('message') + ". " + SYM + " array is being skipped."
+        logger("Warning",MESSAGE)
+        continue
+    else:
+        SGL = sresponse["storageGroupId"]
+
     for SG in SGL:
         #For each SG in a given VMAX, get SRDFA_MBSent metric, sum it up across yesterday and write it out to csv file.
         DATA = payload(SYM,SG)
@@ -99,13 +135,12 @@ for SYM in SYML:
         ssURL = "https://" + LOCATION + "/univmax/restapi/performance/StorageGroup/metrics"
         ssr = requests.post(ssURL, data=json.dumps(DATA, sort_keys=True, indent=4), auth=(USER, PASS), headers=HEADERS, verify = False)
         ssresponse = ssr.json()
-        #print(ssresponse)
 
-        # If response back includes a message instead of expected output, then it should be because the VMAX isn't local or isn't licensed for SMC
-        # So skip the VMAX and move onto the next.
+        # If response back includes a message instead of expected output, then chances are the VMAX isn't local. Skip to next VMAX but report up error.
         test = ssresponse.get('message')
         if test != None:
-            logging.warning(ssresponse.get('message') + " This Array is being skipped.")
+            MESSAGE = ssresponse.get('message') + ". " + SYM + " array is being skipped."
+            logger("Warning",MESSAGE)
             break
 
         RESULT = ssresponse["resultList"]["result"]
