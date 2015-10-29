e882c3e7ca5d0f836ff81b6373c2f40d86e80562
Matt Cowger <matt@cowger.us>
2015-10-28 21:11:59 -0700

    More PEP8 enhancements.  Went from a score of 2.35 to 7.51

--- Reporter.py
+++ Reporter.py
@@ -1,20 +1,26 @@
 #! /usr/bin/env python
 
-import requests, json, time, logging, os, sys, csv
+import requests
+import json
+import time
+import os
+import logging
+import sys
+import csv
 from datetime import datetime, date, timedelta
 from datetime import time as dt_time
-import logging
+
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger()
 
 
 def generate_payload(symmetrix_id, storage_group_id):
     return {
-        "startDate" : unix_ym,
-        "endDate" : unix_midnight,
-        "symmetrixId" : symmetrix_id,
-        "storageGroupId" : storage_group_id,
-        "metrics" : ["SRDFA_MBSent"]
+        "startDate": unix_ym,
+        "endDate": unix_midnight,
+        "symmetrixId": symmetrix_id,
+        "storageGroupId": storage_group_id,
+        "metrics": ["SRDFA_MBSent"]
     }
 
 
@@ -25,7 +31,7 @@ try:
 except Exception:
     logging.info("Ignore messages related to insecure SSL certificates")
 
-#Date
+# Date
 timestamp = date.today() - timedelta(days=1)
 
 # Get the URL from environmentals. Example: "10.241.209.162:8443"
@@ -52,71 +58,83 @@ except KeyError:
 
 if not os.path.isfile('SRDFA_Reporter.csv'):
 
-    with open('SRDFA_Reporter.csv','w') as csvfile:
+    with open('SRDFA_Reporter.csv', 'w') as csvfile:
         writer = csv.writer(csvfile, delimiter=',',
-                                quotechar=',', quoting=csv.QUOTE_MINIMAL)
-        writer.writerow(['24h Period','Array','Storage Group','Total MB Sent by SRDFA'])
+                            quotechar=',', quoting=csv.QUOTE_MINIMAL)
+        writer.writerow(
+            ['24h Period', 'Array', 'Storage Group', 'Total MB Sent by SRDFA'])
 
 
-
-#Get today's midnight
+# Get today's midnight
 midnight = datetime.combine(date.today(), dt_time.min)
-#Convert to unix epoch time
+# Convert to unix epoch time
 unix_midnight = time.mktime(midnight.timetuple())
-#Convert to milliseconds
+# Convert to milliseconds
 unix_midnight *= 1000
-#Remove trailing .0 float from the time
-unix_midnight = str(unix_midnight).replace(".0","")
+# Remove trailing .0 float from the time
+unix_midnight = str(unix_midnight).replace(".0", "")
 
-#Get yesterday's midnight
+# Get yesterday's midnight
 yesterday_midnight = midnight - timedelta(days=1)
-#Convert to unix epoch time
+# Convert to unix epoch time
 unix_ym = time.mktime(yesterday_midnight.timetuple())
-#Convert to milliseconds
+# Convert to milliseconds
 unix_ym *= 1000
-#Remove trailing .0 float from the time
-unix_ym = str(unix_ym).replace(".0","")
+# Remove trailing .0 float from the time
+unix_ym = str(unix_ym).replace(".0", "")
 
 # Get all VMAXs for a given Unisphere Instance
 vURL = "https://" + LOCATION + "/univmax/restapi/provisioning/symmetrix"
 
 try:
-    symmetrix_list = requests.get(vURL, auth=(USER,PASS), verify = False).json()["symmetrixId"]
+    symmetrix_list = requests.get(vURL, auth=(
+        USER, PASS), verify=False).json()["symmetrixId"]
 
 except ValueError:
-    logger.critical('Login failure. $SRUSER and $SRPASS credentials failed. Please confirm the environmental variables are properly set with correct username/password')
+    logger.critical(
+        'Login failure. $SRUSER and $SRPASS credentials failed. Please confirm the environmental variables are properly set with correct username/password')
     sys.exit(1)
 except Exception as e:
-    logger.critical('IP:Port information is incorrect. Please confirm SRUNILOCATION environment variable is properly set and Unisphere is up and running. Detailed Error: {0}'.format(e))
+    logger.critical(
+        'IP:Port information is incorrect. Please confirm SRUNILOCATION environment variable is properly set and Unisphere is up and running. Detailed Error: {}'.format(e))
     sys.exit(1)
 
 # For each VMAX in Unisphere, get the storage Groups
 for symmetrix_id in symmetrix_list:
-    sURL = "https://" + LOCATION + "/univmax/restapi/provisioning/symmetrix/" + symmetrix_id + "/storagegroup"
+    sURL = "https://" + LOCATION + "/univmax/restapi/provisioning/symmetrix/" + \
+        symmetrix_id + "/storagegroup"
 
-    sr = requests.get(sURL, auth=(USER,PASS), verify = False)
+    sr = requests.get(sURL, auth=(USER, PASS), verify=False)
     sresponse = sr.json()
 
-    # Check if a message was returned. If there is a message, report the message and then skip to the next VMAX.
+    # Check if a message was returned. If there is a message, report the
+    # message and then skip to the next VMAX.
 
     if 'message' in sresponse:
-        message = sresponse.get('message') + ". " + symmetrix_id + " array is being skipped."
+        message = sresponse.get('message') + ". " + \
+            symmetrix_id + " array is being skipped."
         logger.warn(message)
         continue
     else:
         storage_group_list = sresponse["storageGroupId"]
 
     for storage_group_id in storage_group_list:
-        #For each SG in a given VMAX, get SRDFA_MBSent metric, sum it up across yesterday and write it out to csv file.
-        DATA = generate_payload(symmetrix_id,storage_group_id)
-        HEADERS = {'content-type': 'application/json','accept':'application/json'}
-        ssURL = "https://" + LOCATION + "/univmax/restapi/performance/StorageGroup/metrics"
-        ssr = requests.post(ssURL, data=json.dumps(DATA, sort_keys=True, indent=4), auth=(USER, PASS), headers=HEADERS, verify = False)
+        # For each SG in a given VMAX, get SRDFA_MBSent metric, sum it up
+        # across yesterday and write it out to csv file.
+        data = generate_payload(symmetrix_id, storage_group_id)
+        headers = {
+            'content-type': 'application/json',
+            'accept': 'application/json'}
+        ss_url = "https://" + LOCATION + "/univmax/restapi/performance/StorageGroup/metrics"
+        ssr = requests.post(ss_url, data=json.dumps(data, sort_keys=True, indent=4), auth=(USER, PASS), headers=headers, verify=False)
         ssresponse = ssr.json()
 
-        # If response back includes a message instead of expected output, then chances are the VMAX isn't local. Skip to next VMAX but report up error.
+        # If response back includes a message instead of expected output, then
+        # chances are the VMAX isn't local. Skip to next VMAX but report up
+        # error.
         if 'message' in ssresponse:
-            message = ssresponse.get('message') + ". " + symmetrix_id + " array is being skipped."
+            message = ssresponse.get('message') + ". " + \
+                symmetrix_id + " array is being skipped."
             logger.warn(message)
             break
 
@@ -124,13 +142,15 @@ for symmetrix_id in symmetrix_list:
 
         total = 0
 
-        #Grab Total MB Sent for a given SG
+        # Grab Total MB Sent for a given SG
         for item in result:
-           total +=item['SRDFA_MBSent']
+            total += item['SRDFA_MBSent']
 
-        print("Writing to file: {0}, {1}, {2}".format(timestamp, symmetrix_id, storage_group_id))
+        print("Writing to file: {0}, {1}, {2}".format(
+            timestamp, symmetrix_id, storage_group_id))
 
         with open('SRDFA_Reporter.csv', 'a') as csvfile:
             writer = csv.writer(csvfile, delimiter=',',
-                                    quotechar=',', quoting=csv.QUOTE_MINIMAL)
-            writer.writerow([str(timestamp), symmetrix_id, storage_group_id, str(total)])
+                                quotechar=',', quoting=csv.QUOTE_MINIMAL)
+            writer.writerow([str(timestamp), symmetrix_id,
+                             storage_group_id, str(total)])
