990c74ce7a19f50d3b7ce1ba32889fced704a848
Ryan Clair <ryan.clair@gmail.com>
2015-10-30 15:28:14 -0500

    Added Additional Logging Capabilities

--- Reporter.py
+++ Reporter.py
@@ -10,8 +10,32 @@ import csv
 from datetime import datetime, date, timedelta
 from datetime import time as dt_time
 
-logging.basicConfig(level=logging.DEBUG)
-logger = logging.getLogger()
+# Get logging level from env var SRLOGGINGLEVEL.
+# If it doesn't exist, default to "INFO"
+# Ensure that the value is capitalized
+user_level = os.getenv('SRLOGGINGLEVEL', "INFO").upper()
+valid_response = ["DEBUG", "INFO", "WARNING", "CRITICAL"]
+
+if user_level not in valid_response:
+    # If the user put incorrect response, report the error and set the value back to default.
+    user_level = "INFO"
+    logging.basicConfig(
+        level=logging.getLevelName(user_level),
+        format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+        filename='SRDFA_Reporter_Error_Log.csv')
+    console_write = logging.StreamHandler()
+    console_write.setLevel(logging.getLevelName(user_level))
+    logger = logging.getLogger('').addHandler(console_write)
+    logger.warning("SRLOGGINGLEVEL contains an invalid value. Valid values are: DEBUG, INFO, WARNING, and CRITICAL")
+else:
+    # Otherwise use the value provided by user.
+    logging.basicConfig(
+        level=logging.getLevelName(user_level),
+        format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+        filename='SRDFA_Reporter_Error_Log.csv')
+    console_write = logging.StreamHandler()
+    console_write.setLevel(logging.getLevelName(user_level))
+    logger = logging.getLogger('').addHandler(console_write)
 
 
 def generate_payload(symmetrix_id, storage_group_id):
@@ -27,9 +51,10 @@ def generate_payload(symmetrix_id, storage_group_id):
 # Disable warnings from untrusted server certificates
 try:
     from requests.packages.urllib3.exceptions import InsecureRequestWarning
+
     requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
-except Exception:
-    logging.info("Ignore messages related to insecure SSL certificates")
+except Exception as e:
+    logging.info("Ignore messages related to insecure SSL certificates. Error: {}".format(e))
 
 # Date
 timestamp = date.today() - timedelta(days=1)
@@ -38,20 +63,23 @@ timestamp = date.today() - timedelta(days=1)
 try:
     LOCATION = os.environ['SRUNILOCATION']
 except KeyError:
-    logger.critical('Need an IP:Port. Please set the environment variable SRUNILOCATION to IP:Port of Unisphere using either "export SRUNILOCATION=IP:PORT" (linux command) or "set SRUINLOCATION=IP:PORT" (Windows command)')
+    logger.critical('Need an IP:Port. Please set the environment variable SRUNILOCATION to IP:Port of Unisphere \
+    using either "export SRUNILOCATION=IP:PORT" (linux command) or "set SRUINLOCATION=IP:PORT" (Windows command)')
     sys.exit(1)
 
 # Get credentials for Unisphere login from environmentals
 try:
     USER = os.environ['SRUSER']
 except KeyError:
-    logger.critical('Need a username. Please set the environment variable SRUSER to the username of Unisphere using either "export SRUSER=username" (linux command) or "set SRUSER=username" (Windows command)')
+    logger.critical('Need a username. Please set the environment variable SRUSER to the username of Unisphere using \
+    either "export SRUSER=username" (linux command) or "set SRUSER=username" (Windows command)')
     sys.exit(1)
 
 try:
     PASS = os.environ['SRPASS']
 except KeyError:
-    logger.critical('Need a password. Please set the environment variable SRPASS to the password of Unisphere using either "export SRUSER=password" (linux command) or "set SRUSER=password" (Windows command)')
+    logger.critical('Need a password. Please set the environment variable SRPASS to the password of Unisphere using \
+    either "export SRUSER=password" (linux command) or "set SRUSER=password" (Windows command)')
     sys.exit(1)
 
 # Get current directory the script is running in
@@ -87,22 +115,44 @@ unix_ym = str(unix_ym).replace(".0", "")
 vURL = "https://" + LOCATION + "/univmax/restapi/provisioning/symmetrix"
 
 try:
-    symmetrix_list = requests.get(vURL, auth=(
-        USER, PASS), verify=False).json()["symmetrixId"]
+    symmetrix_list_response = requests.get(vURL, auth=(
+        USER, PASS), verify=False).json()
+
+    if 'message' in symmetrix_list_response:
+        # I've seen an issue where Unisphere returns a message of "No Symmetrix's found.
+        print("yes")
+        message = symmetrix_list_response.get('message')
+        logger.critical(message)
+        sys.exit(1)
+
+    else:
+        # Assuming no messages, store the list of VMAX's into symmetrix_list
+        symmetrix_list = symmetrix_list_response["symmetrixId"]
+
+except TimeoutError:
+    logger.critical(
+        "Unisphere couldn't be reached. Please confirm SRUNILOCATION environment variable is properly set and \
+             Unisphere is up and running."
+    )
 
 except ValueError:
     logger.critical(
-        'Login failure. $SRUSER and $SRPASS credentials failed. Please confirm the environmental variables are properly set with correct username/password')
+        "Login failure. $SRUSER and $SRPASS credentials failed. Please confirm the environmental variables are \
+        properly set with correct username/password")
     sys.exit(1)
 except Exception as e:
+
     logger.critical(
-        'IP:Port information is incorrect. Please confirm SRUNILOCATION environment variable is properly set and Unisphere is up and running. Detailed Error: {}'.format(e))
+        "There was an unexpected error. Details: {}".format(e))
     sys.exit(1)
+
+# Print to screen the list of the available SYMs
+print(symmetrix_list)
 
 # For each VMAX in Unisphere, get the storage Groups
 for symmetrix_id in symmetrix_list:
     sURL = "https://" + LOCATION + "/univmax/restapi/provisioning/symmetrix/" + \
-        symmetrix_id + "/storagegroup"
+           symmetrix_id + "/storagegroup"
 
     sr = requests.get(sURL, auth=(USER, PASS), verify=False)
     sresponse = sr.json()
@@ -111,9 +161,7 @@ for symmetrix_id in symmetrix_list:
     # message and then skip to the next VMAX.
 
     if 'message' in sresponse:
-        message = sresponse.get('message') + ". " + \
-            symmetrix_id + " array is being skipped."
-        logger.warn(message)
+        logger.warn(sresponse.get('message') + ". " + symmetrix_id + " array is being skipped.")
         continue
     else:
         storage_group_list = sresponse["storageGroupId"]
@@ -124,20 +172,22 @@ for symmetrix_id in symmetrix_list:
         data = generate_payload(symmetrix_id, storage_group_id)
         headers = {
             'content-type': 'application/json',
-            'accept': 'application/json'}
+            'accept': 'application/json'
+        }
         ss_url = "https://" + LOCATION + "/univmax/restapi/performance/StorageGroup/metrics"
-        ssr = requests.post(ss_url, data=json.dumps(data, sort_keys=True, indent=4), auth=(USER, PASS), headers=headers, verify=False)
+        ssr = requests.post(ss_url, data=json.dumps(data, sort_keys=True, indent=4),
+                            auth=(USER, PASS), headers=headers, verify=False)
         ssresponse = ssr.json()
 
         # If response back includes a message instead of expected output, then
         # chances are the VMAX isn't local. Skip to next VMAX but report up
         # error.
         if 'message' in ssresponse:
-            message = ssresponse.get('message') + ". " + \
-                symmetrix_id + " array is being skipped."
+            message = ssresponse.get('message') + ". " + symmetrix_id + " array is being skipped."
             logger.warn(message)
             break
 
+        print(ssresponse)
         result = ssresponse["resultList"]["result"]
 
         total = 0
@@ -146,8 +196,8 @@ for symmetrix_id in symmetrix_list:
         for item in result:
             total += item['SRDFA_MBSent']
 
-        print("Writing to file: {0}, {1}, {2}".format(
-            timestamp, symmetrix_id, storage_group_id))
+        print("Writing to file: {0}, {1}, {2}, {3}".format(
+            timestamp, symmetrix_id, storage_group_id, total))
 
         with open('SRDFA_Reporter.csv', 'a') as csvfile:
             writer = csv.writer(csvfile, delimiter=',',
