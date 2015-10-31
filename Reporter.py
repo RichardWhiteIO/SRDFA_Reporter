7a23e31ff2b922b293eb0c8997a3eada2b05b5b4
Ryan Clair <ryan.clair@gmail.com>
2015-10-31 01:56:27 -0500

    Cleaning up the code

--- Reporter.py
+++ Reporter.py
@@ -13,7 +13,7 @@ from datetime import time as dt_time
 # Get logging level from env var SRLOGGINGLEVEL.
 # If it doesn't exist, default to "INFO"
 # Ensure that the value is capitalized
-user_level = os.getenv('SRLOGGINGLEVEL', "INFO").upper()
+user_level = os.getenv('SRLOGGINGLEVEL', "WARNING").upper()
 valid_response = ["DEBUG", "INFO", "WARNING", "CRITICAL"]
 wrong = False
 
@@ -44,37 +44,27 @@ except Exception as e:
 
 # Get the URL from environmentals. Example: "10.241.209.162:8443"
 try:
-    LOCATION = os.environ['SRUNILOCATION']
+    location = os.environ['SRUNILOCATION']
 except KeyError:
-    logger.critical('Need an IP:Port. Please set the environment variable SRUNILOCATION to IP:Port of Unisphere \
-    using either "export SRUNILOCATION=IP:PORT" (linux command) or "set SRUINLOCATION=IP:PORT" (Windows command)')
+    logger.critical('Need an IP:Port. Please set the environment variable SRUNILOCATION to IP:Port of Unisphere '
+                    'using either "export SRUNILOCATION=IP:PORT" (linux command) or "set SRUINLOCATION=IP:PORT" '
+                    '(Windows command)')
     sys.exit(1)
 
 # Get credentials for Unisphere login from environmentals
 try:
-    USER = os.environ['SRUSER']
+    user = os.environ['SRUSER']
 except KeyError:
-    logger.critical('Need a username. Please set the environment variable SRUSER to the username of Unisphere using \
-    either "export SRUSER=username" (linux command) or "set SRUSER=username" (Windows command)')
+    logger.critical('Need a username. Please set the environment variable SRUSER to the username of Unisphere using'
+                    'either "export SRUSER=username" (linux command) or "set SRUSER=username" (Windows command)')
     sys.exit(1)
 
 try:
-    PASS = os.environ['SRPASS']
+    password = os.environ['SRPASS']
 except KeyError:
-    logger.critical('Need a password. Please set the environment variable SRPASS to the password of Unisphere using \
-    either "export SRUSER=password" (linux command) or "set SRUSER=password" (Windows command)')
+    logger.critical('Need a password. Please set the environment variable SRPASS to the password of Unisphere using '
+                    'either "export SRUSER=password" (linux command) or "set SRUSER=password" (Windows command)')
     sys.exit(1)
-
-# Get current directory the script is running in
-
-if not os.path.isfile('SRDFA_Reporter.csv'):
-
-    with open('SRDFA_Reporter.csv', 'w') as csvfile:
-        writer = csv.writer(csvfile, delimiter=',',
-                            quotechar=',', quoting=csv.QUOTE_MINIMAL)
-        writer.writerow(
-            ['24h Period', 'Array', 'Storage Group', 'Total MB Sent by SRDFA'])
-
 
 def generate_payload(symmetrix_id, storage_group_id):
     return {
@@ -106,12 +96,20 @@ unix_ym *= 1000
 # Remove trailing .0 float from the time
 unix_ym = str(unix_ym).replace(".0", "")
 
+# If SRDFA_Reporter.csv file doesn't exist, create it with a header.
+if not os.path.isfile('SRDFA_Reporter.csv'):
+    with open('SRDFA_Reporter.csv', 'w') as csvfile:
+        writer = csv.writer(csvfile, delimiter=',',
+                            quotechar=',', quoting=csv.QUOTE_MINIMAL)
+        writer.writerow(
+            ['24h Period', 'Array', 'Storage Group', 'Total MB Sent by SRDFA'])
+
 # Get all VMAXs for a given Unisphere Instance
-vURL = "https://" + LOCATION + "/univmax/restapi/provisioning/symmetrix"
+vURL = "https://" + location + "/univmax/restapi/provisioning/symmetrix"
 
 try:
     symmetrix_list_response = requests.get(vURL, auth=(
-        USER, PASS), verify=False).json()
+        user, password), verify=False).json()
 
     if 'message' in symmetrix_list_response:
         # I've seen an issue where Unisphere returns a message of "No Symmetrix's found.
@@ -126,8 +124,8 @@ try:
 
 except ValueError:
     logger.critical(
-        "Login failure. $SRUSER and $SRPASS credentials failed. Please confirm the environmental variables are \
-        properly set with correct username/password")
+        "Login failure. $SRUSER and $SRPASS credentials failed. Please confirm the environmental variables are "
+        "properly set with correct username/password")
     sys.exit(1)
 except Exception as e:
 
@@ -138,56 +136,54 @@ except Exception as e:
 
 # For each VMAX in Unisphere, get the storage Groups
 for symmetrix_id in symmetrix_list:
-    sURL = "https://" + LOCATION + "/univmax/restapi/provisioning/symmetrix/" + \
+    sURL = "https://" + location + "/univmax/restapi/provisioning/symmetrix/" + \
            symmetrix_id + "/storagegroup"
 
-    sr = requests.get(sURL, auth=(USER, PASS), verify=False)
-    sresponse = sr.json()
+    storage_group_response = requests.get(sURL, auth=(user, password), verify=False).json()
 
     # Check if a message was returned. If there is a message, report the
     # message and then skip to the next VMAX.
 
-    if 'message' in sresponse:
-        logger.warn(sresponse.get('message') + ". " + symmetrix_id + " array is being skipped.")
+    if 'message' in storage_group_response:
+        logger.warn(storage_group_response.get('message') + ". " + symmetrix_id + " array is being skipped.")
         continue
 
     else:
-        storage_group_list = sresponse["storageGroupId"]
+        storage_group_list = storage_group_response["storageGroupId"]
 
     for storage_group_id in storage_group_list:
         # For each SG in a given VMAX, get SRDFA_MBSent metric, sum it up
         # across yesterday and write it out to csv file.
-        data = generate_payload(symmetrix_id, storage_group_id)
+        payload = generate_payload(symmetrix_id, storage_group_id)
         headers = {
             'content-type': 'application/json',
             'accept': 'application/json'
         }
-        ss_url = "https://" + LOCATION + "/univmax/restapi/performance/StorageGroup/metrics"
-        ssr = requests.post(ss_url, data=json.dumps(data, sort_keys=True, indent=4),
-                            auth=(USER, PASS), headers=headers, verify=False)
-        ssresponse = ssr.json()
+        ss_url = "https://" + location + "/univmax/restapi/performance/StorageGroup/metrics"
+        srdfa_response = requests.post(ss_url, data=json.dumps(payload),
+                            auth=(user, password), headers=headers, verify=False).json()
 
         # If response back includes a message instead of expected output, then
         # chances are the VMAX isn't local. Skip to next VMAX but report up
         # error.
-        if 'message' in ssresponse:
-            message = ssresponse.get('message') + ". " + symmetrix_id + " array is being skipped."
+        if 'message' in srdfa_response:
+            message = srdfa_response.get('message') + ". " + symmetrix_id + " array is being skipped."
             logger.warn(message)
             break
 
-        result = ssresponse["resultList"]["result"]
+        result = srdfa_response["resultList"]["result"]
 
-        total = 0
+        srdfa_total = 0
 
         # Grab Total MB Sent for a given SG
         for item in result:
-            total += item['SRDFA_MBSent']
+            srdfa_total += item['SRDFA_MBSent']
 
         print("Writing to file: {0}, {1}, {2}, {3}".format(
-            timestamp, symmetrix_id, storage_group_id, total))
+            timestamp, symmetrix_id, storage_group_id, srdfa_total))
 
         with open('SRDFA_Reporter.csv', 'a') as csvfile:
             writer = csv.writer(csvfile, delimiter=',',
                                 quotechar=',', quoting=csv.QUOTE_MINIMAL)
-            writer.writerow([str(timestamp), symmetrix_id,
-                             storage_group_id, str(total)])
+            writer.writerow([timestamp, symmetrix_id,
+                             storage_group_id, str(srdfa_total)])
